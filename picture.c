2f5c75d05a0eca968cf5588054d74341c762d994
NikiNikov <110698748+NikiNikov@users.noreply.github.com>
2022-08-11 02:43:45 +0300

    Update picture.c

--- picture.c
+++ picture.c
@@ -1,18 +1,19 @@
+#include <stdio.h>
 #define N 15
 #define M 13
-#include <stdio.h>
 
 void transform(int *buf, int **matr, int n, int m);
 void make_picture(int **picture, int n, int m);
 void reset_picture(int **picture, int n, int m);
-
+void print_picture(int **picture, int n, int m);
 int main()
 {
    int picture_data[N][M];
    int *picture[N];
-   transform(picture_data[0], (int**)picture, N, M);
+   transform(*picture_data, picture, N, M);
 
    make_picture(picture, N, M);
+   print_picture(picture, N, M);
 }
 
 void make_picture(int **picture, int n, int m)
@@ -29,46 +30,72 @@ void make_picture(int **picture, int n, int m)
                           { 0, 0, 0, 0, 0 } };
 
    reset_picture(picture, n, m);
-    tree_trunk[0]++;
-    sun_data[0][0]++;
-    tree_foliage[0]++;
-    frame_h[0]++;
 
-   int length_frame_w = sizeof(frame_w) / sizeof(frame_w[0]);
-    int length_frame_h = sizeof(frame_h) / sizeof(frame_h[0]);
+   int length_tree_tr = sizeof(tree_trunk) / sizeof(tree_trunk[0]);
+   int length_tree_fol = sizeof(tree_foliage) / sizeof(tree_foliage[0]);
+   for(int i = 0; i < length_tree_tr; i++) {
+       picture[3][i+2] = tree_foliage[i];
+       picture[4][i+2] = tree_foliage[i];
+       picture[i+2][3] = tree_foliage[i];
+       picture[i+2][4] = tree_foliage[i];
+   }
 
-   for (int i = 0; i < length_frame_w; i++)
-   {
-      picture[0][i] = frame_w[i];
-       picture[length_frame_w/2][i] = frame_w[i];
-      picture[length_frame_w - 1][i] = frame_w[i];
+   for(int i = 0; i < length_tree_fol; i++) {
+       picture[6+length_tree_tr][i+2] = tree_trunk[i];
+       picture[i+2+length_tree_tr][3] = tree_trunk[i];
+       picture[i+2+length_tree_tr][4] = tree_trunk[i];
 
    }
+
+   for (int i = 1; i < n/2 - 1; i++)
+   {
+       for (int j = m/2 + 1 ; j < n - 1; j++)
+       {
+           picture[i][j] = sun_data[i-1][j - m/2 - 1];
+       }
+   }
+
+   int length_frame_w = sizeof(frame_w) / sizeof(frame_w[0]);
+   int length_frame_h = sizeof(frame_h) / sizeof(frame_h[0]);
+
+   for (int i = 0; i < length_frame_w; i++)
+    {
+       picture[0][i] = frame_w[i];
+       picture[n/2][i] = frame_w[i];
+       picture[n - 1][i] = frame_w[i];
+    }
 
     for (int i = 0; i < length_frame_h; i++)
     {
        picture[i][0] = frame_h[i];
-       picture[i][length_frame_h - 1] = frame_h[i];
+       picture[i][m/2] = frame_h[i];
+       picture[i][m - 1] = frame_h[i];
 
     }
+}
 
 
-    for (int i = 0; i < 15; ++i) {
-      for (int j = 0; j < 13; ++j)
-          printf("%d ",picture[i][j]);
+void print_picture(int **picture, int n, int m){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", picture[i][j]);
+        }
         printf("\n");
     }
 }
 
+
 void reset_picture(int **picture, int n, int m)
 {
-   for (int i = 0; i < m; i++)
-   {
-      for (int j = 0; j < n; j++)
-      {
-         picture[i][j] = 0;
-      }
-   }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            picture[i][j] = 0;
+        }
+    }
 }
 
 void transform(int *buf, int **matr, int n, int m)
